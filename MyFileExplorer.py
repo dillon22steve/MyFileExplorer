b617e23098e5626e9b31df31ad1b61f14a9ecb85
Steven Dillon <std42400@uga.edu>
2023-12-03 15:20:24 -0500

    Updated the images and added comments
    Updated the images so that the file paths are not hard coded. Also commented a lot of the code to clean it up.

--- MyFileExplorer.py
+++ MyFileExplorer.py
@@ -1,5 +1,6 @@
 import os
 from os import path
+from pathlib import Path
 import shutil
 
 from tkinter import *
@@ -42,39 +43,47 @@ class MyFileExplorer():
         dirTextBox.insert('1.0', currDir)
 
 
-        #folder = PhotoImage(file = 'C:\\Users\\dillo\\Documents\GitHub\\MyFileExplorer\\FileFolder2.gif').subsample(50, 50)
-        folder = PhotoImage(file = 'C:\\Users\\dillo\\Documents\GitHub\\MyFileExplorer\\FileFolder2.gif')
-        fileImg = PhotoImage(file = 'C:\\Users\\dillo\\Documents\GitHub\\MyFileExplorer\\File.gif')
+        #thisDir stores the name of the directory that the images are in. The following two lines
+        #load the images into the folder and fileImg variables.
+        thisDir = os.path.dirname(os.path.abspath(__file__))
+        folder = PhotoImage(file = (thisDir + '\\FileFolder2.gif'))
+        fileImg = PhotoImage(file = (thisDir + '\\File.gif'))
 
 
-
+        #Initialize the tree that displays the files and subdirectories in the user's home directory.
         homeDirTree = ttk.Treeview(homeDirFrame)
         homeDirTree.place(x = 0, y = 0, width = 190, height = 300)
         homeDirTree.config(selectmode="browse")
 
+        #Initializes the scrollbar that the user can use to scroll through the filed and subdirectories
+        #in their home directory.
         hdtScrollbar = ttk.Scrollbar(homeDirFrame, orient="vertical", command=homeDirTree.yview)
         hdtScrollbar.place(x = 190, y = 0, width = 10, height = 300)
         homeDirTree.config(yscrollcommand = hdtScrollbar.set)
 
 
 
-
+        #Initialize the tree that displays the files and subdirectories in the current directory.
         currDirTree = ttk.Treeview(currDirFrame)
         currDirTree.place(x = 0, y = 0, width = 560, height = 300)
         currDirTree.config(selectmode='browse')
 
+        #Initializes the scrollbar that the user can use to scroll through the filed and subdirectories
+        #in their current directory.
         cdtScrollbar = ttk.Scrollbar(currDirFrame, orient="vertical", command=currDirTree.yview)
         cdtScrollbar.place(x = 560, y = 0, width = 10, height = 300)
         currDirTree.config(yscrollcommand = cdtScrollbar.set)
 
 
-
+        #Loops through the files and subdirectories in the user's home directory and displays them in
+        #the homeDirTree
         def initHomeDir():
             i = 0
             for file in os.scandir(homeDir):
                 fileName = file.name
                 path = homeDir + "\\" + file.name
-                    
+                
+                #If file is a directory, it displays the folder image next to it, else it displays a file image.
                 if os.path.isdir(path):
                     homeDirTree.insert('', i, i, text = fileName, image = folder)
                     i = i + 1
@@ -89,8 +98,13 @@ class MyFileExplorer():
         def updateCurrDirTree(event):
             updateCwd(False)
 
+        #When the user clicks a folder, it updates the current directory to that folder
+        #and then reconfigures the tree to display the new directory's contents
         def updateCwd(isHomeDir):
             fileName = ""
+
+            #If the user clicks a folder in their home directory, updates currDir to the
+            #home directory, else it updates currDir to the new directory
             if isHomeDir == True:
                 currItem = homeDirTree.focus()
                 fileName = homeDirTree.item(currItem).get("text")
@@ -100,19 +114,24 @@ class MyFileExplorer():
                 fileName = currDirTree.item(currItem).get("text")
                 currDir = os.getcwd()
 
+            #If the item clicked is a file, return. Else, change the directory.
             if path.isfile(fileName):
                 return
             else:
+                #Gets the new directory and chdir's into the new directory.
                 nextDir = currDir + "\\" + fileName
                 os.chdir(nextDir)
                 currDir = os.getcwd()
 
                 replaceCurrDirTree(nextDir)
 
+                #Update the text box that displays the path to the directory.
                 dirTextBox.delete('1.0', '1.0 lineend')
                 dirTextBox.insert('1.0', currDir)
         
+        #Updates the tree to display the new directories files and subdirectories
         def replaceCurrDirTree(newDir):
+            #Deletes the current contents of the tree.
             for item in currDirTree.get_children():
                 currDirTree.delete(item)
 
@@ -120,6 +139,8 @@ class MyFileExplorer():
             for file in os.scandir(newDir):
                 fileName = file.name
                 path = newDir + "\\" + fileName
+
+                #If file is a directory, it displays the folder image next to it, else it displays a file image.
                 if os.path.isdir(path):
                     currDirTree.insert('', 'end', i, text=fileName, image=folder)
                     i = i + 1
@@ -132,6 +153,7 @@ class MyFileExplorer():
 
         initHomeDir()
 
+        #When the user clicks a folder in the tree, it updates the tree
         homeDirTree.bind('<<TreeviewSelect>>', updateHomeDirTree)
         currDirTree.bind('<<TreeviewSelect>>', updateCurrDirTree)
 
